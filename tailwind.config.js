e72082893f017dff129c7c0d13863d96c6bbdbab
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-10 13:02:34 +0300

    fixed bg img

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url(./src/assets/neon-light-min.jpg)",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
   },
   plugins: [],
 };
