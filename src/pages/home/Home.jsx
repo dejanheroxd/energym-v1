decb4093b2d8f2f6eef90d9c9c6bbe978fe4d46e
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-10 12:30:34 +0300

    width fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <div className="relative justify-center items-center flex-row flex h-full bg-woman">
-        <div className=" h-96 w-68 text-center">
+        <div className=" h-96 w-80 text-center">
           <h1 className="text-white font-semibold text-5xl">
             Welcome to Energym
           </h1>
