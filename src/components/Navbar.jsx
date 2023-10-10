7616fc8e2ea509a293fe64744545cb3b2a3e66c0
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-10 12:26:40 +0300

    added home screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { AnimatePresence, motion, useAnimate, stagger } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Navbar() {
   const [navActive, setNavActive] = useState(false);
@@ -67,7 +67,7 @@ function Navbar() {
     },
     open: {
       transition: {
-        delayChildren: 0.3,
+        delayChildren: 0.2,
         staggerChildren: 0.09,
         staggerDirection: 1,
       },
@@ -77,28 +77,29 @@ function Navbar() {
   function onNavActive() {
     setNavActive((prev) => !prev);
   }
-  console.log(navActive);
 
   return (
-    <header className="h-14 bg-sky-400 sm:flex sm:justify-between sm:h-20 sm:px-2  sm:items-center">
+    <header className="h-14 flex items-center bg-white sm:flex sm:justify-between sm:h-20 sm:px-2  sm:items-center">
       <Link to="/">
-        <p className="font-bold">ENERGYM</p>
+        <p className="font-bold text-xl pl-2">ENERGYM</p>
       </Link>
       <button
         onClick={() => onNavActive()}
-        className="absolute top-4 right-3 z-50 sm:hidden"
+        className="absolute top-[23px] right-3 z-50 sm:hidden"
       >
-        <svg
-          width="29"
-          height="21"
-          viewBox="0 0 29 21"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="13" height="3" fill="black" />
-          <rect x="16" y="18" width="13" height="3" fill="black" />
-          <rect y="9" width="29" height="3" fill="black" />
-        </svg>
+        <div>
+          <span
+            className={`bg-black h-[3px] duration-300  w-6 block mb-1 ${
+              navActive ? "rotate-45 translate-y-1" : ""
+            }`}
+          ></span>
+
+          <span
+            className={`bg-black h-[3px] duration-300  w-6 block ${
+              navActive ? "-rotate-45 translate-y-[-3px]" : ""
+            }`}
+          ></span>
+        </div>
       </button>
       <nav>
         <AnimatePresence>
@@ -109,7 +110,7 @@ function Navbar() {
               animate="animate"
               exit="exit"
               className="
-         sm:flex sm:relative origin-top sm:flex-row justify-center flex text-3xl sm:text-base text-center    flex-col gap-y-7 sm:gap-x-3 bg-gray-200 top-0 right-0 bottom-0 z-40 left-0 py-40 px-8 sm:px-0 sm:py-0 sm:bg-opacity-0 sm:left-0 h-full fixed font-semibold"
+         sm:flex sm:relative origin-top sm:flex-row justify-center flex text-3xl sm:text-base text-center    flex-col gap-y-7 sm:gap-x-3 bg-white top-0 right-0 bottom-0 z-40 left-0 py-40 px-8 sm:px-0 sm:py-0 sm:bg-opacity-0 sm:left-0 h-full fixed font-semibold"
             >
               <motion.div
                 variants={containerVars}
@@ -148,80 +149,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-// import { useState } from "react";
-// import { Link, NavLink } from "react-router-dom";
-// import { motion } from "framer-motion";
-
-// function Navbar() {
-//   const [navActive, setNavActive] = useState(false);
-
-//   const menuVars = {
-//     initial: {
-//       scaleY: 0,
-//     },
-//     animate: {
-//       scaleY: 1,
-//     },
-//     exit: {
-//       scaleY: 0,
-//     },
-//   };
-
-//   function onNavActive() {
-//     setNavActive((prev) => !prev);
-//   }
-//   console.log(navActive);
-
-//   return (
-//     <motion.header
-//       variants={menuVars}
-//       initial="initial"
-//       animate="animate"
-//       exit="exit"
-//       className="h-14 bg-sky-400 sm:flex sm:justify-between sm:h-20 sm:px-2  sm:items-center"
-//     >
-//       <Link to="/">
-//         <p className="font-bold">ENERGYM</p>
-//       </Link>
-//       <button
-//         onClick={() => onNavActive()}
-//         className="absolute top-4 right-3 z-50 sm:hidden"
-//       >
-//         <svg
-//           width="29"
-//           height="21"
-//           viewBox="0 0 29 21"
-//           fill="none"
-//           xmlns="http://www.w3.org/2000/svg"
-//         >
-//           <rect width="13" height="3" fill="black" />
-//           <rect x="16" y="18" width="13" height="3" fill="black" />
-//           <rect y="9" width="29" height="3" fill="black" />
-//         </svg>
-//       </button>
-//       <nav>
-//         <ul
-//           className={`${
-//             navActive ? "translate-y-0" : ""
-//           } sm:flex sm:relative sm:flex-row justify-center flex text-3xl sm:text-base text-center  sm:translate-x-0 sm:translate-y-0 translate-y-[-100%] transition-transform ease-out duration-700 flex-col gap-y-7 sm:gap-x-3 bg-gray-200 top-0 right-0 bottom-0 left-0 py-40 px-8 sm:px-0 sm:py-0 sm:bg-opacity-0 sm:left-0 h-full fixed font-semibold`}
-//         >
-//           <li>
-//             <NavLink to="about">ABOUT</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="services">SERVICES</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="membership">MEMBERSHIP</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="contact">CONTACT</NavLink>
-//           </li>
-//         </ul>
-//       </nav>
-//     </motion.header>
-//   );
-// }
-
-// export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,24 @@
+// import blackWoman from "../../assets/black-woman.webp";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="relative justify-center items-center flex-row flex h-full bg-woman">
+        <div className=" h-96 w-68 text-center">
+          <h1 className="text-white font-semibold text-5xl">
+            Welcome to Energym
+          </h1>
+          <h2 className="text-white font-semibold text-2xl mt-6">
+            The fitness facility for all of your needs
+          </h2>
+          <button className="mt-9 text-black border-none px-7 py-4 rounded-full bg-white">
+            Join Now
+          </button>
+        </div>
+      </div>
+      <div className=" h-72">heyy</div>
+    </>
+  );
 }
 
 export default Home;
