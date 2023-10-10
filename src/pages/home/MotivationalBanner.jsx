0705084ff4e0971fcea2116d94c3f5f34ad8d63e
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-10 16:37:31 +0300

    borde deleted

--- src/pages/home/MotivationalBanner.jsx
+++ src/pages/home/MotivationalBanner.jsx
@@ -20,7 +20,7 @@ function MotivationalBanner() {
               alt=""
             />
           </div>
-          <div className="text-center pb-4 border h-96 flex flex-col justify-center px-4 items-center">
+          <div className="text-center pb-4  h-96 flex flex-col justify-center px-4 items-center">
             <p className="font-semibold text-2xl">The Onyx Experience</p>
             <p className="mt-5">
               {`At Energym, we're not just a fitness facility; we're your dedicated partner in helping you achieve and maintain a healthier, more active lifestyle. Our mission is to empower you with the knowledge and support you need to reach your fitness goals.`}
@@ -38,7 +38,7 @@ function MotivationalBanner() {
               alt=""
             />
           </div>
-          <div className="text-center pb-4 border h-96 flex flex-col justify-center px-4 items-center">
+          <div className="text-center pb-4  h-96 flex flex-col justify-center px-4 items-center">
             <p className="font-semibold text-2xl">Fitness Is for Everyone</p>
             <p className="mt-5">
               {`We passionately believe that fitness is a journey meant for everyone. Whether you're an experienced athlete or taking your first steps towards a healthier lifestyle, our gym welcomes individuals of all backgrounds and fitness levels.`}
