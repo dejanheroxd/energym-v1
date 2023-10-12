a545ddb57c3a574b81e884db8b10e488fd17bf54
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-12 13:24:20 +0300

    contact form and bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import NavBar from "./components/Navbar";
+// import Home from "./pages/home/Home";
+// import About from "./pages/about/About";
+// import Membership from "./pages/membership/Membership";
+// import Services from "./pages/services/Services";
+// import Contact from "./pages/contact/Contact";
+// import BackToTopButton from "./components/BackToTopButton";
+// import Footer from "./components/Footer";
+
+// function App() {
+//   return (
+//     <div className="pt-16">
+//       <Router>
+//         <NavBar />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/membership" element={<Membership />} />
+//           <Route path="/services" element={<Services />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//         <BackToTopButton />
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import NavBar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
@@ -8,11 +44,22 @@ import Contact from "./pages/contact/Contact";
 import BackToTopButton from "./components/BackToTopButton";
 import Footer from "./components/Footer";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="pt-16">
       <Router>
         <NavBar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
