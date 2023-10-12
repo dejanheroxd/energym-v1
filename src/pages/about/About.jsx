4c7f4467defd0579bb410788fd90deafc427af42
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-12 11:42:24 +0300

    text fix

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -17,19 +17,19 @@ function About() {
         <div className="text-center px-5 pt-10">
           <p className="font-semibold text-xl pb-3">Why We Choose Fitness</p>
           <p className="pb-5">
-            It's our passion to help you live a healthier, more active life.
+            {`It's our passion to help you live a healthier, more active life.
             Fitness isn't just about working out; it's about feeling better,
-            gaining more energy, and taking care of yourself.
+            gaining more energy, and taking care of yourself.`}
           </p>
           <p>
-            Our team is made up of experts who are dedicated to your well-being.
+            {`Our team is made up of experts who are dedicated to your well-being.
             We provide a range of services and classes to make fitness
             accessible to everyone. Whether you're new to exercise or have
             experience, we're here to support you. Our journey began with a
             simple idea, the idea that everyone deserves a chance to be
             healthier. We're not just another gym; we're a community that
             welcomes you with open arms. Let us help you achieve your fitness
-            goals and lead you towards a better, more active lifestyle.
+            goals and lead you towards a better, more active lifestyle.`}
           </p>
         </div>
       </div>
