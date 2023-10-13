249b995a8ac980cea98066d8dd099fb9af063567
dejanheroxd <127946013+dejanheroxd@users.noreply.github.com>
2023-10-13 23:20:03 +0300

    animtion fix cookie

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { X } from "phosphor-react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -13,48 +13,51 @@ function Cookie() {
   }
 
   const cookieVars = {
-    initial: { opacity: 0, y: 50, transition: { duration: 1.4 } },
-    animate: { opacity: 1, y: 0, transition: { duration: 1.4 } },
-    exit: { opacity: 0, y: 50, transition: { duration: 1.4 } },
+    initial: { opacity: 0, y: 50, transition: { duration: 1 } },
+    animate: { opacity: 1, y: 0, transition: { duration: 1 } },
+    exit: { opacity: 0, y: 50, transition: { duration: 0.3 } },
   };
 
   return (
-    cookieVisible && (
-      <motion.div
-        variants={cookieVars}
-        initial="initial"
-        animate="animate"
-        className=" w-full h-[290px] fixed bg-white bottom-0 flex justify-center"
-      >
-        <div className=" w-80 h-full flex flex-col pt-10 lg:items-center lg:flex-row lg:w-[800px] lg:justify-between">
-          <p className="text-gray-600 pb-6 text-sm lg:w-80">
-            {t("cookie.header")}
-          </p>
-          <div className="flex flex-col gap-y-2 lg:w-80 lg:justify-center">
-            <button
-              onClick={() => onCookieClick()}
-              className="bg-gymrose text-white py-2"
-            >
-              {t("cookie.reject")}
-            </button>
-            <button
-              onClick={() => onCookieClick()}
-              className="bg-gymrose text-white py-2"
-            >
-              {t("cookie.accept")}
-            </button>
+    <AnimatePresence>
+      {cookieVisible && (
+        <motion.div
+          variants={cookieVars}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          className=" w-full h-[290px] fixed bg-white bottom-0 flex justify-center"
+        >
+          <div className=" w-80 h-full flex flex-col pt-10 lg:items-center lg:flex-row lg:w-[800px] lg:justify-between">
+            <p className="text-gray-600 pb-6 text-sm lg:w-80">
+              {t("cookie.header")}
+            </p>
+            <div className="flex flex-col gap-y-2 lg:w-80 lg:justify-center">
+              <button
+                onClick={() => onCookieClick()}
+                className="bg-gymrose text-white py-2"
+              >
+                {t("cookie.reject")}
+              </button>
+              <button
+                onClick={() => onCookieClick()}
+                className="bg-gymrose text-white py-2"
+              >
+                {t("cookie.accept")}
+              </button>
+            </div>
           </div>
-        </div>
-        <button>
-          <X
-            onClick={() => onCookieClick()}
-            size={19}
-            weight="bold"
-            className="absolute top-3 right-3"
-          />
-        </button>
-      </motion.div>
-    )
+          <button>
+            <X
+              onClick={() => onCookieClick()}
+              size={19}
+              weight="bold"
+              className="absolute top-3 right-3"
+            />
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
