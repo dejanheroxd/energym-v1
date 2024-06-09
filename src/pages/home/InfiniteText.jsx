55912e99d68eeb4a3c9a394ee78d9042adbc2174
dejan <yugihohgx2@gmail.com>
2024-06-09 15:34:38 +0300

    commit

--- src/pages/home/InfiniteText.jsx
+++ src/pages/home/InfiniteText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function InfiniteText() {
   return (
     <div className="w-screen text-white flex justify-center">
-      <div className="flex text-[0.75rem] items-center h-[50vh] xl:pb-[11rem] w-[100vw] overflow-hidden max-w-[100vw">
+      <div className="flex text-[0.6rem] items-center h-[50vh] xl:pb-[11rem] w-[100vw] overflow-hidden max-w-[100vw">
         <div className="scroll">
           <div className="RightToLeft mb-14 xl:mb-16">
             <p>Transform Your Life</p>
