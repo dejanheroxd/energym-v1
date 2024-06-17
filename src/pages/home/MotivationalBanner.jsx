cd96c242f0421322ae2df9d53bdab4c725cd27c2
dejan <yugihohgx2@gmail.com>
2024-06-17 09:00:05 +0300

    commit

--- src/pages/home/MotivationalBanner.jsx
+++ src/pages/home/MotivationalBanner.jsx
@@ -13,7 +13,7 @@ function MotivationalBanner() {
   return (
     <div className="relative lg:h-[1290px]">
       <div className="lg:grid-row-2 lg:grid lg:mx-20 lg:absolute bg-white lg:w-[92%] xl:h-[1300px] lg:top-[-160px]">
-        <div className="h-[770px] lg:h-full">
+        <div className="h-[790px] sm:h-[770px] lg:h-full">
           <div className="lg:grid grid-cols-2">
             <div className="h-96 lg:h-[640px] overflow-hidden">
               <LazyLoadImage
@@ -29,7 +29,7 @@ function MotivationalBanner() {
                 alt=""
               />
             </div>
-            <div className="text-center xl:text-left lg:text-left pb-4 h-96 xl:h-full flex lg:h-full flex-col justify-center px-4 items-center">
+            <div className="text-center xl:text-left lg:text-left pb-4 h-96 xl:h-full flex lg:h-full flex-col justify-center px-4 items-center pt-9 sm:pt-0">
               <motion.div
                 initial={{ opacity: 0 }}
                 whileInView={{
@@ -50,7 +50,7 @@ function MotivationalBanner() {
             </div>
           </div>
         </div>
-        <div className="h-[770px] lg:h-full">
+        <div className="h-[790px] sm:h-[770px] lg:h-full">
           <div className="lg:grid grid-cols-2 ">
             <svg
               width="354"
@@ -87,7 +87,7 @@ function MotivationalBanner() {
                 alt=""
               />
             </div>
-            <div className="text-center lg:h-full pb-4 xl:text-left lg:text-left h-96 flex flex-col justify-center order-1 px-4 items-center">
+            <div className="text-center pt-9 sm:pt-0 lg:h-full pb-4 xl:text-left lg:text-left h-96 flex flex-col justify-center order-1 px-4 items-center">
               <motion.div
                 initial={{ opacity: 0 }}
                 whileInView={{
